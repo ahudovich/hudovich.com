3787275473057f18d47378a2e326d0c64c7d8da1
Andrei Hudovich <andrei@hudovich.com>
2025-08-26 11:08:35 +0200

    chore: add posthog

---
+++ instrumentation-client.ts
@@ -0,0 +1,8 @@
+import posthog from 'posthog-js'
+import { env } from './env'
+
+posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+  api_host: `/api2/`,
+  ui_host: env.NEXT_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-05-24',
+})
