b8933b122f4889a71751153683f99f47d068dc52
Andrei Hudovich <andrei@hudovich.com>
2025-11-20 11:41:07 +0100

    chore: fix eslint in lint-staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,10 +1,5 @@
-import path from 'node:path'
-
-const eslintCommand = (filenames) =>
-  `eslint --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
-
 const config = {
-  '*.{ts,tsx}': [() => 'tsc -p tsconfig.json --noEmit', eslintCommand],
+  '*.{ts,tsx}': [() => 'tsc -p tsconfig.json --noEmit', 'eslint .'],
   '*.{js,mjs,cjs,ts,tsx,css}': ['prettier --check'],
 }
 
