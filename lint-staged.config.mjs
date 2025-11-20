714a8186695beb786c9bf7079d0133598c00d7be
Andrei Hudovich <andrei@hudovich.com>
2025-11-20 11:34:03 +0100

    chore: remove next lint command

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,7 +1,7 @@
 import path from 'node:path'
 
 const eslintCommand = (filenames) =>
-  `next lint --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
+  `eslint --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
 
 const config = {
   '*.{ts,tsx}': [() => 'tsc -p tsconfig.json --noEmit', eslintCommand],
