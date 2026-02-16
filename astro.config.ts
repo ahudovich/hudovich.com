147ac5f5b3942d28e17188a4c9945b4dda0c74a5
Andrei Hudovich <andrei@hudovich.com>
2026-02-16 21:14:20 +0100

    chore: add type safe env variables

--- astro.config.ts
+++ astro.config.ts
@@ -9,4 +9,24 @@ export default defineConfig({
     process.env.VERCEL_ENV === "preview"
       ? `https://${process.env.VERCEL_URL}`
       : process.env.PUBLIC_BASE_URL,
+
+  env: {
+    validateSecrets: true,
+    schema: {
+      PUBLIC_ENV: envField.enum({
+        context: "server",
+        access: "secret",
+        values: ["development", "preview", "production"],
+      }),
+      PUBLIC_POSTHOG_HOST: envField.string({
+        context: "client",
+        access: "public",
+        url: true,
+      }),
+      PUBLIC_POSTHOG_KEY: envField.string({
+        context: "client",
+        access: "public",
+      }),
+    },
+  },
 });
