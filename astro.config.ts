64304891c5491a8ad305dcf9f1793cdde59f993f
Andrei Hudovich <andrei@hudovich.com>
2026-02-17 22:39:01 +0100

    fix: fix vercel preview url resolving #3

--- astro.config.ts
+++ astro.config.ts
@@ -3,14 +3,12 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 
-const vercelPreviewURL = process.env.VERCEL_URL ?? process.env.VERCEL_BRANCH_URL
-
 export default defineConfig({
   integrations: [mdx(), sitemap()],
 
   site:
-    process.env.VERCEL_ENV === 'preview' && vercelPreviewURL
-      ? `https://${vercelPreviewURL}`
+    process.env.VERCEL_ENV === 'preview'
+      ? `https://${process.env.VERCEL_URL}`
       : process.env.PUBLIC_BASE_URL,
 
   trailingSlash: 'never',
