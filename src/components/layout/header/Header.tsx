6615270f968fdc023c02375c1e347466f891c632
Andrei Hudovich <andrei@hudovich.com>
2026-02-27 18:29:03 +0100

    chore: extract and hide Navigation component

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,23 +1,8 @@
 import Link from 'next/link'
-import { QuillWrite01Icon, Wrench01Icon } from '@hugeicons/core-free-icons'
 import { Container } from '@/components/layout/Container'
 import { Logo } from '@/components/logos/Logo'
 import { Button } from '@/components/ui/Button'
-import { Icon } from '@/components/ui/Icon'
 import { HeaderThemeToggle } from './HeaderThemeToggle'
-
-const links = [
-  {
-    href: '/posts',
-    label: 'Posts',
-    icon: QuillWrite01Icon,
-  },
-  {
-    href: '/tools',
-    label: 'Tools',
-    icon: Wrench01Icon,
-  },
-] as const
 
 export function Header(props: React.ComponentProps<'header'>) {
   return (
@@ -36,25 +21,6 @@ export function Header(props: React.ComponentProps<'header'>) {
             </Link>
           }
         />
-
-        <nav>
-          <ul className="flex items-center gap-3 sm:gap-4">
-            {links.map((link) => (
-              <li key={link.href}>
-                <Button
-                  nativeButton={false}
-                  variant="ghost"
-                  render={
-                    <Link href={link.href}>
-                      <Icon icon={link.icon} />
-                      {link.label}
-                    </Link>
-                  }
-                />
-              </li>
-            ))}
-          </ul>
-        </nav>
 
         <div className="-mr-2 ml-auto">
           <HeaderThemeToggle />
